58404604b7634dec8b9d009176af3367585ef9f0
Louis Leduc <leduclouis64@hotmail.fr>
2018-11-29 12:43:41 +0100

    update issues

---
+++ src/static/issueManagement.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('button').click(function() {
+        $.ajax({
+            url: '/addIssue',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response){
+              var json = JSON.parse(response);
+              $('.card-deck').append('<div class="card bg-primary text-center"><div class="card-header">'+json.name+'</div><div class="card-body text-center"><p class="card-text">'+json.id+'</p></div></div>');
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+      });
+});
