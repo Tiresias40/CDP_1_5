327b7f060163d1f75c57671a80da46a77bbfb758
nezoutcarl <nezout.carlos@gmail.com>
2018-12-07 16:33:52 +0100

    front fixed

--- src/static/issueManagement.js
+++ src/static/issueManagement.js
@@ -13,4 +13,14 @@ $(function() {
             }
         });
       });
+
+      $(document).ready(function(){
+        const project_id = $('#currentProjectId').text();
+        $('#navGroup').append('<li class="nav-item"><a class="nav-link"'+
+          ' id="issueNav" href="/projects/id/'+project_id+'/issuesPage">Issues</a></li>'+
+          '<li class="nav-item"><a class="nav-link" id="sprintNav" '+
+          'href="/projects/id/'+project_id+'/sprintsPage">'+
+          'Sprints</a></li><li class="nav-item"><a class="nav-link"'+
+          ' id="devNav" href="/projects/id/'+project_id+'/managementPage">Management</a></li>');
+      });
 });
--- src/static/sprintManagement.js
+++ src/static/sprintManagement.js
@@ -6,7 +6,7 @@ $(function() {
             data: $('form').serialize(),
             type: 'POST',
             success: function(response){
-            
+
             },
             error: function(error) {
                 console.log(error);
@@ -20,11 +20,21 @@ $(function() {
             data: $('form').serialize(),
             type: 'DELETE',
             success: function(response){
-            
+
             },
             error: function(error) {
                 console.log(error);
             }
         });
+    });
+
+    $(document).ready(function(){
+      const project_id = $('#currentProjectId').text();
+      $('#navGroup').append('<li class="nav-item"><a class="nav-link"'+
+        ' id="issueNav" href="/projects/id/'+project_id+'/issuesPage">Issues</a></li>'+
+        '<li class="nav-item"><a class="nav-link" id="sprintNav" '+
+        'href="/projects/id/'+project_id+'/sprintsPage">'+
+        'Sprints</a></li><li class="nav-item"><a class="nav-link"'+
+        ' id="devNav" href="/projects/id/'+project_id+'/managementPage">Management</a></li>');
     });
 });
